51773a7ffc6768b88aed798c8330a595506fefad
Collin Stedman <cstedman@princeton>
2014-11-16 03:21:08 -0500

    Saving traffic by the minute

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
 var io = require('socket.io').listen(7717);
 io.set('heartbeat timeout', 99999);
 
@@ -6,12 +11,38 @@ var hosts = {};
 io.sockets.on('connection', function (socket) {
   socket.on('host', function (data) {
     if (hosts[data.host] == undefined) {
-      hosts[data.host] = 1;
+      var time = new Date();
+      var second = time.getSeconds();
+      console.log('second of new entry is ' + second);
+      hosts[data.host] = [];
+      var size = 60;
+      while(size--) hosts[data.host].push({ timestamp: time, count: 0 });
+      hosts[data.host][second].count = 1;
       console.log(data.host + " " + 1);
+      console.error(data.host + " " + 1);
     }
     else {
-      hosts[data.host] += 1;
-      console.log(data.host + " " + hosts[data.host]);
+      var time = new Date();
+      for (var i = 0; i < 60; i++) {
+        if (time - hosts[data.host][i].timestamp > 60000) {
+          hosts[data.host][i].count = 0;
+        }
+      }
+      var second = time.getSeconds();
+      hosts[data.host][second].count++;
+      hosts[data.host][second].timestamp = time;
+      var sum = hosts[data.host].reduce(
+        function(prev,current) {
+          return current.count + prev;
+        }, 0);
+      console.log(data.host + " " + sum);
+      console.error(data.host + " " + sum);
     }
   });
 });
+
+app.get('/data', function(req, res) {
+  res.send(hosts);
+});
+
+app.listen(7718);
